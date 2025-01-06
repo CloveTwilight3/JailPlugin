59ea49657e6ccaedd31448e3b3fe2cb27951b1e7
ApfelTeeSaft <91074565+ApfelTeeSaft@users.noreply.github.com>
2025-01-06 13:39:03 +0100

    Update JailPlugin.java

--- src/main/java/com/example/JailPlugin.java
+++ src/main/java/com/example/JailPlugin.java
@@ -3,6 +3,7 @@ package com.example.jailplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import org.bukkit.Location;
 import org.bukkit.World;
